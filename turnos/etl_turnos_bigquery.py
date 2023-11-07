0960c1a152e87aa3e1e9518592a216b9bc185454
Jorge Londoño <jorge.londono@coopsana.com.co>
2023-11-07 10:34:58 -0500

    Add function for convert columns

--- turnos/etl_turnos_bigquery.py
+++ turnos/etl_turnos_bigquery.py
@@ -26,9 +26,33 @@ validator_column = 'id_turno'
 
 TABLA_BIGQUERY = f'{project_id_product}.{dataset_id_turnos}.{table_name_turnos}'
 
-        
+def convert_columns_str(df):
+    df.tiempoEntreAtencion = df.tiempoEntreAtencion.astype(str)
+    df.tiempoEntreAtencion = [row.replace('days','') for row in df.tiempoEntreAtencion]
+    return df
+
+def convert_columns_date(df):
+    df.fecha_real = pd.to_datetime(df.fecha_real, errors='coerce')
+    df.fecha_nacimiento = pd.to_datetime(df.fecha_nacimiento, errors='coerce')
+    return df
+
+def convert_columns_number(df):
+    df.edad.fillna(0, inplace=True)
+    df.atencion.fillna(0, inplace=True)
+    df.t_taquilla.fillna(0, inplace=True)
+
+    df.edad  = df_turnos.edad.astype(int)
+    df.atencion = df_turnos.atencion.astype(int)
+    df.t_taquilla = df_turnos.t_taquilla.astype(int)
+    return df
+
 # Leer datos
 df_turnos_bd = func_process.load_df_server(SQL_TURNOS_BD, 'reportes')   
+
+# Convert columns 
+df_turnos_bd = convert_columns_str(df_turnos_bd)
+df_turnos_bd = convert_columns_date(df_turnos_bd)
+df_turnos_bd = convert_columns_number(df_turnos_bd)
 
 # Obtener datos no duplicados
 valores_unicos = tuple(map(int,df_turnos_bd[validator_column]))
