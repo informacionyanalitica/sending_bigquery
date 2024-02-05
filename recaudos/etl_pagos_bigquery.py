5e4ada08436656065f22fd0982b19d45317803dd
Jorge Londono <jorge.londono@coopsana.com.co>
2024-02-05 08:27:34 -0500

    update SQL pagos etl

--- recaudos/etl_pagos_bigquery.py
+++ recaudos/etl_pagos_bigquery.py
@@ -9,21 +9,8 @@ import func_process
 import load_bigquery as loadbq
 
 
-SQL_PAGOS_BD = """SELECT p.id_pago,p.factura,p.valorCobrar,p.numeroAutorizacion,
-            p.numeroIdentificacionPaciente,p.estadoPago AS idEstadoPago,c.descripcion_estadoOrden AS estadoPago,
-            p.tipo_pago,tp.tipo_pago AS tipoPago,p.resolucion,p.numero_adicional,
-            p.codigoTipoCobro,cp.descripcion_codigoTipoCobro AS tipoCobro,dl.hv_identificacion AS documentoUsuarioCobra,
-            p.usuarioCobra,p.codigo_sede_cobra AS codigoSedeCobra,ms.nombre_sede AS nombreSedeCobra,
-            p.detalle_pago,p.id_servicio_pago_particular AS idServicioPagoParticular,csp.nombreServicioPagoParticular,
-            p.fechaPago,p.fecha_cierre
-            FROM recaudo_sura.pagos AS p 
-            JOIN recaudo_sura.estadoOrden AS c ON c.id_estadoOrden = p.estadoPago
-            JOIN central.maestra_tipo_pago AS tp ON tp.id_tipo_pago = p.tipo_pago
-            JOIN recaudo_sura.codigoTipoCobro AS cp ON cp.id_codigoTipoCobro=p.codigoTipoCobro
-            JOIN miturno.maestra_sedes AS ms ON ms.codigo_sede_interno = p.codigo_sede_cobra
-            JOIN recaudo_sura.codigoServiciosPagosParticulares AS csp ON csp.idServicioPagoParticular = p.id_servicio_pago_particular
-            JOIN intranet.usuariosAccesos ua ON ua.usuario = p.usuarioCobra
-            JOIN hojas_vida.datos_laborales dl ON dl.hv_id_experiencia = ua.id_hv_experiencia
+SQL_PAGOS_BD = """SELECT *
+                FROM pagosHistoricosView AS P
             WHERE date(p.fechaPago) >= adddate(curdate(), interval -7 day)
                 """
                 
