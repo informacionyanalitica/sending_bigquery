be884f775505e7e24f202c952d8297fdd64a2e3b
Jorge Londono <jorge.londono@coopsana.com.co>
2024-08-30 15:30:57 -0500

    Update file save logic

--- ayudas_diagnosticas/etl_gestion_clinica.py
+++ ayudas_diagnosticas/etl_gestion_clinica.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 PATH_DRIVE = os.environ.get("PATH_DRIVE")
 PATH_TOOLS = os.environ.get("PATH_TOOLS")
+PATH_ETL = os.environ.get("PATH_ETL")
 path = os.path.abspath(PATH_TOOLS)
 sys.path.insert(1,path)
 import func_process
@@ -24,17 +25,17 @@ date_load = (datetime.now() - timedelta(days=1))
 date_execution = date_load.date()
 MONTH_NAME = date_load.strftime('%B').capitalize()
 PATH_GESTION_CLINICA = {"path_folder":"Gestión Clínica"} 
-PATH_FILE_SAVE = f'{PATH_DRIVE}/Gestión Clínica/{MONTH_NAME}/'
+PATH_FILE_SAVE = f'{PATH_ETL}/etl/files/{MONTH_NAME}/'
 NAME_FILE = str(date_execution)+'.xlsx'
 
 
 
 RENAME_COLUMN_REQUIRED = ['identificacion_paciente','nombre_paciente','apellido_paciente','celular','email','orden_cltech','nombre_prueba','resultado',
                           'refmin','refmax','fecha_validacion','fecha_ingreso','orden_sura','identificacion_medico',
-                          'nombre_medico','cargo_medico','sede_medico',
+                          'nombre_medico','cargo_medico','sede_medico','email_medico'
                           ]
 COLUMNS_REQUIRED = ['HISTORIA','nombre_paciente','apellido_paciente','celular','email','_order', 'name', 'result', 'refmin', 'refmax','fechaValidacion',
-                        'entryDate', 'autorizacionSura','C_MEDICO', 'MEDICO', 'cargo_gestal', 'SEDE_MEDICO']
+                        'entryDate', 'autorizacionSura','C_MEDICO', 'MEDICO', 'cargo_gestal', 'SEDE_MEDICO','email_medico']
 
 # BIGQUERY
 project_id_product = '`ia-bigquery-397516'
@@ -53,9 +54,10 @@ SQL_PERFILES_LABORATORIO =  f"""
                 AND sl.refmin != 'undefined'
                 """
 SQL_LABORATORIO = f"""SELECT distinct lb.ORDEN_SEDE,lb.HISTORIA,lb.NOMBRE,lb.C_MEDICO,lb.MEDICO,lb.cargo_gestal,lb.SEDE_MEDICO,
-                        cp.celular,UPPER(cp.email) AS email
+                        cp.celular,UPPER(cp.email) AS email,ea.email as email_medico
                     FROM `ia-bigquery-397516.ayudas_diagnosticas.laboratorio_bd_view` as lb
                     JOIN `ia-bigquery-397516.pacientes.capita` as cp on cp.identificacion_paciente = lb.HISTORIA
+                    LEFT JOIN `ia-bigquery-397516.empleados.activos` as ea on ea.identificacion = lb.C_MEDICO
                     WHERE DATE(lb.FECHA) >= DATE_SUB('{date_execution}', INTERVAL 1 MONTH) 
                     AND lb.sede_medico != 'EXTERNO'
                     AND lb.ORDEN_SEDE != '1'"""
@@ -70,39 +72,24 @@ def read_dataset():
     except Exception as err:
         print(err)
 
-def get_id_folder():
-    try:
-        # GET ID FOLDER
-        id_folder = filesGD.getIdsGoogleSheet(PATH_GESTION_CLINICA)
-        id_folder_month = id_folder[id_folder.name == MONTH_NAME]
-        url_drive = f'https://drive.google.com/drive/u/0/folders/{id_folder_month.id.values[0]}'
-        return url_drive
-    except Exception as err:
-        print(err)
-        
 def validate_folder():
     try:
         pattern = os.path.join(PATH_FILE_SAVE)
         folder_exists = glob.glob(pattern)
         if not folder_exists:
             os.mkdir(PATH_FILE_SAVE)
-        url_drive = get_id_folder()
-        return url_drive
     except Exception as err:
         print(err)
 
 
-
 def validate_save_file(df_gestion_medica):
     try:
-        url_drive = validate_folder()
+        validate_folder()
         df_gestion_medica.to_excel(PATH_FILE_SAVE+NAME_FILE, index=False) 
         pattern_files = os.path.join(PATH_FILE_SAVE,NAME_FILE)
         files_exists = glob.glob(pattern_files)
         if not files_exists:
             raise ValueError(f"No se encontraron archivos con el patrón: {pattern_files}")
-        else:
-            print(url_drive)
     except Exception as err:
         print(err)
    
@@ -115,6 +102,8 @@ def get_merge(df_perfiles,df_laboratorio_sin_duplicados):
         return df_gestion_medica
     except Exception as err:
         print(err)
+
+
 
 
 df_laboratorio_sin_duplicados,df_perfiles = read_dataset()
